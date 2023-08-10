c28f726fea6585ef0891dcbca77bc2a30a88cfc4
DDTE2 <$".(}9|$Q#Zmv}tGc4x%0nvu1;E[`S)bGh9>
2023-08-10 17:44:49 +1000

    7.10.7
    - В скрипт удаления модаа добавлено удаление ярлыка с рабочего стола.

--- Algorithms/Delete.py
+++ Algorithms/Delete.py
@@ -1,13 +1,14 @@
 from os.path import abspath
 from os import walk, remove, system
 from shutil import rmtree
+from Algorithms.Shortcut import Shortcut
 
 def Delete_All():
     # Получаем путь к папке проекта
     project = abspath(__file__).split('\\')[:-2]
     project = '\\'.join(project)
 
-    # Удаляем всё
+    # Удаляем все файлы проекта
     for path, dirnames, filenames in walk(project):
         for dir in dirnames:
             try:
@@ -19,6 +20,12 @@ def Delete_All():
                 remove(path + '/' + file)
             except:
                 pass
+
+    # Удаляем ярлык с рабочего стола
+    shortcut = Shortcut()
+    if shortcut.Check_System():
+        if shortcut.Check_Shortcut():
+            shortcut.Delete()
 
     # Открываем папку проекта, чтобы пользователь удалил то, что осталось
     system('explorer.exe ' + project)
